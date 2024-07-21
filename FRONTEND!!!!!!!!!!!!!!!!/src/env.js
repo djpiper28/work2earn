bbf2de9467bdf23cab3c35e0734c2243a20dee74
Danny Piper <djpiper28@gmail.com>
2024-07-21 10:33:14 +0100

    fix: no db

--- FRONTEND!!!!!!!!!!!!!!!!/src/env.js
+++ FRONTEND!!!!!!!!!!!!!!!!/src/env.js
@@ -3,17 +3,6 @@ import { z } from "zod";
 
 export const env = createEnv({
   /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
-  server: {
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-  },
-
-  /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
@@ -27,7 +16,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
